48df8df6a42cca92c6bddc2ca92163728c2a3329
mariomolinero78 <126725660+mariomolinero78@users.noreply.github.com>
2023-12-16 11:15:37 -0600

    ultima version MARIO

--- app/src/main/java/com/example/pedidosexpress/views/Repartidor/AdministrarCuentaActivity.kt
+++ app/src/main/java/com/example/pedidosexpress/views/Repartidor/AdministrarCuentaActivity.kt
@@ -1,12 +1,32 @@
 package com.example.pedidosexpress.views.Repartidor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.pedidosexpress.R
+import com.google.android.material.snackbar.Snackbar
 
 class AdministrarCuentaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_administrar_cuenta)
+
+        // Ejemplo de cómo mostrar un Snackbar al hacer clic en un botón (reemplaza con tus elementos):
+        val btnGuardarCambios: Button = findViewById(R.id.btnGuardarCambios)
+        btnGuardarCambios.setOnClickListener {
+            guardarCambiosEnCuenta()
+        }
+    }
+
+    // Método para guardar los cambios en la cuenta
+    private fun guardarCambiosEnCuenta() {
+        // Aquí implementa la lógica para guardar los cambios en la cuenta
+        val adminCuentaLayout: View = findViewById(R.id.AdministrarCuenta) // Reemplaza con el ID correcto si es necesario
+        Snackbar.make(
+            adminCuentaLayout,
+            "Cambios guardados exitosamente",
+            Snackbar.LENGTH_SHORT
+        ).show()
     }
 }
--- app/src/main/java/com/example/pedidosexpress/views/Repartidor/FavoritosRepartidorActivity.kt
+++ app/src/main/java/com/example/pedidosexpress/views/Repartidor/FavoritosRepartidorActivity.kt
@@ -1,12 +1,35 @@
 package com.example.pedidosexpress.views.Repartidor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.pedidosexpress.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class FavoritosRepartidorActivity : AppCompatActivity() {
+
+    private lateinit var tvFavoritosTitle: TextView
+    private lateinit var ivFavorito1: ImageView
+    private lateinit var ivFavorito2: ImageView
+    private lateinit var bottomNavigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favoritos_repartidor)
+
+        // Inicialización de vistas
+        tvFavoritosTitle = findViewById(R.id.tvFavoritosTitle)
+        ivFavorito1 = findViewById(R.id.ivFavorito1)
+        ivFavorito2 = findViewById(R.id.ivFavorito2)
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+
+        // Configuración del título de la sección
+        tvFavoritosTitle.text = "Mis Favoritos"
+
+        // Configuración de imágenes de favoritos (puedes cargar tus imágenes aquí)
+        ivFavorito1.setImageResource(R.drawable.ic_favorito_1)
+        ivFavorito2.setImageResource(R.drawable.ic_favorito_2)
+
     }
 }
