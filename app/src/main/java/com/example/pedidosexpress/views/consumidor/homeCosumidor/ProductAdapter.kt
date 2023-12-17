6c422394e42839f22707837274f2270f3dfdbcd9
jesus58k <tiburon58k@gmail.com>
2023-12-17 15:56:52 -0600

    ultima version jesus

---
+++ app/src/main/java/com/example/pedidosexpress/views/consumidor/homeCosumidor/ProductAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.pedidosexpress.views.consumidor.homeCosumidor
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.pedidosexpress.R
+
+class ProductAdapter(context: Context, resource: Int, objects: List<ProductsFragment.Product>) :
+    ArrayAdapter<ProductsFragment.Product>(context, resource, objects) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val inflater = LayoutInflater.from(context)
+        val itemView = inflater.inflate(R.layout.lista_prodcutos, parent, false)
+
+        // Obtén el producto en la posición actual
+        val product = getItem(position)
+
+        // Configura los elementos de la vista con los datos del producto
+        val productName: TextView = itemView.findViewById(R.id.nombreProducto)
+        val productPrice: TextView = itemView.findViewById(R.id.precio)
+        val productImage: ImageView = itemView.findViewById(R.id.imageProducto)
+        val addToCartButton: Button = itemView.findViewById(R.id.btnagregar)
+
+        productName.text = product?.name
+        productPrice.text = product?.price
+        productImage.setImageResource(product?.imageResId ?: 0)
+
+        // Configura la lógica del botón (agregar al carrito) según tus necesidades
+
+        return itemView
+    }
+}
--- app/src/main/java/com/example/pedidosexpress/views/consumidor/homeCosumidor/ProductsFragment.kt
+++ app/src/main/java/com/example/pedidosexpress/views/consumidor/homeCosumidor/ProductsFragment.kt
@@ -1,26 +1,48 @@
 package com.example.pedidosexpress.views.consumidor.homeCosumidor
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.fragment.app.Fragment
 import com.example.pedidosexpress.R
-class ProductsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+class ProductsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_products, container, false)
+        val view = inflater.inflate(R.layout.fragment_products, container, false)
+
+        // Obtén la referencia al ListView
+        val productListView: ListView = view.findViewById(R.id.productListView)
+
+        // Crea un array de ejemplo de productos (puedes reemplazar esto con tu lógica de obtención de datos)
+        val productList = generateSampleProducts()
+
+        // Crea un adaptador personalizado
+        val productAdapter =
+            ProductAdapter(requireContext(), R.layout.lista_prodcutos, productList)
+
+        // Establece el adaptador en el ListView
+        productListView.adapter = productAdapter
+
+        return view
     }
 
-    companion object {
+    // Método para generar productos de ejemplo (puedes reemplazarlo con tu propia lógica)
+    private fun generateSampleProducts(): List<Product> {
+        val productList = mutableListOf<Product>()
+        productList.add(Product("Producto 1", "$10", R.drawable.logo_1))
 
+        // Agrega más productos según sea necesario
+        return productList
     }
+
+    // Clase de datos para representar un producto
+    data class Product(val name: String, val price: String, val imageResId: Int)
+
 }
