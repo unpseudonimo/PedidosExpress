9fb7b7ad0bb746fecf69cbea91badb71aa46c270
AJfreeblack00110000 <143929391+unpseudonimo@users.noreply.github.com>
2023-12-23 02:19:00 -0600

    Revert "Delete app/src/main/java/com/example/pedidosexpress/views/repartidor directory"
    This reverts commit 944e7355ee4efbada5eaab10d45facb1be4c3f5c.

---
+++ app/src/main/java/com/example/pedidosexpress/views/repartidor/CuentaRepartidor.kt
@@ -0,0 +1,38 @@
+package com.example.pedidosexpress.views.Repartidor
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.pedidosexpress.R
+import com.example.pedidosexpress.views.consumidor.BottomNavigationHandler
+import com.example.pedidosexpress.views.main.MainActivity
+import com.example.pedidosexpress.views.main.RegistrarCuenta
+
+class CuentaRepartidor : AppCompatActivity() {
+    private lateinit var btnIniciarSesionConsumidor: Button
+    private lateinit var btnRegistrarCuenta: Button
+
+    private lateinit var bottomNavigationHandler: BottomNavigationHandler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cuenta_repartidor) // Establece el layout de la actividad
+        // Inicializa el botón después de setContentView
+        btnRegistrarCuenta = findViewById<Button>(R.id.btnRegistrarCuenta)
+
+        bottomNavigationHandler = BottomNavigationHandler(this)
+
+        btnIniciarSesionConsumidor.setOnClickListener {
+            startActivity(Intent(this@CuentaRepartidor,  MainActivity::class.java))
+        }
+       /* btnRegistrarCuenta.setOnClickListener {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<RegistrarCuenta>(R.id.FragmentContainer)
+            }
+        }*/
+    }
+}
