4045b596c12f4eff2c10e2fd26199b4016a66071
AJfreeblack00110000 <143929391+unpseudonimo@users.noreply.github.com>
2023-12-25 19:37:52 -0600

    Nvegacion de abajo para el repartidor

--- app/src/main/java/com/example/pedidosexpress/views/repartidor/CuentaRepartidor.kt
+++ app/src/main/java/com/example/pedidosexpress/views/repartidor/CuentaRepartidor.kt
@@ -10,26 +10,16 @@ import com.example.pedidosexpress.views.main.MainActivity
 //
 class CuentaRepartidor : AppCompatActivity() {
     private lateinit var btnIniciarSesionConsumidor: Button
-    private lateinit var btnRegistrarCuenta: Button
 
-    private lateinit var bottomNavigationHandlerConsumidor: BottomNavigationHandlerConsumidor
+    private lateinit var bottomNavigationHandler: BottomNavigationHandlerRepartidor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cuenta_repartidor) // Establece el layout de la actividad
         // Inicializa el botón después de setContentView
-        btnRegistrarCuenta = findViewById<Button>(R.id.btnRegistrarCuenta)
 
-        bottomNavigationHandlerConsumidor = BottomNavigationHandlerConsumidor(this)
+        bottomNavigationHandler = BottomNavigationHandlerRepartidor(this)
 
-        btnIniciarSesionConsumidor.setOnClickListener {
-            startActivity(Intent(this@CuentaRepartidor,  MainActivity::class.java))
-        }
-        /* btnRegistrarCuenta.setOnClickListener {
-             supportFragmentManager.commit {
-                 setReorderingAllowed(true)
-                 add<RegistrarCuenta>(R.id.FragmentContainer)
-             }
-         }*/
+
     }
 }
