82b87dce6be7dbfa9c365dbc000e0a5e4a0293b1
AJfreeblack00110000 <143929391+unpseudonimo@users.noreply.github.com>
2024-01-12 00:24:24 -0600

    se trabajo con la entrega del pedido

--- app/src/main/java/com/example/pedidosexpress/views/repartidor/HomeRFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pedidosexpress.views.repartidor
-
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.viewmodel.CreationExtras
-
-class HomeRFragment : Fragment() {
-    override val defaultViewModelCreationExtras: CreationExtras
-        get() = super.defaultViewModelCreationExtras
-}
